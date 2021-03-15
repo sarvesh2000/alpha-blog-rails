d1640e5ce1b60c7d0711e9c5f0e0b8c9fad0d899
Sarvesh <sarvesh4232@gmail.com>
2021-03-15 19:55:00 +0530

    Create new Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,15 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save()
+    redirect_to article_path(@article)
+    # or we can write like this
+    #redirect_to @article
+  end
 end
