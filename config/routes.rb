34dcb0171b58183dcff01ebda7c382ce66ae7e28
Sarvesh <sarvesh4232@gmail.com>
2021-03-16 11:55:12 +0530

    Delete Operation,Links for nav,Home and About Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  root "pages#home"
+  get "about", to: "pages#about"
+  resources :articles
 end
